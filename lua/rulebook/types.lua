7a4187d2c7b0d341747b8c0353605858d56d9b78
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2023-09-22 02:25:17 +0200

    fix: diagnostic typing

--- lua/rulebook/types.lua
+++ lua/rulebook/types.lua
@@ -1,5 +1,5 @@
 ---@class diagnostic nvim diagnostic https://neovim.io/doc/user/diagnostic.html#diagnostic-structure
 ---@field message string
----@field source string -- linter of LSP name
----@field code string -- rule id
+---@field source? number -- linter of LSP name
+---@field code? string -- rule id
 ---@field bufnr number
