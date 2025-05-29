21f73a334c49876b0de2e2ea08ed3a4264a39813
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2025-05-29 11:45:38 +0200

    refactor: cleanup rule docs module

--- lua/rulebook/data/rule-docs.lua
+++ lua/rulebook/data/rule-docs.lua
@@ -1,62 +1,63 @@
--- INFO the key must be named exactly like `diagnostic.source` (case-sensitive!)
--- * string value: `%s` will be replaced with the rule id
--- * function value: will be called with the diagnostic object
--- * `false`: disable rule docs, just use the fallback
---------------------------------------------------------------------------------
-
 ---some providers save the links to the docs in the diagnostic object
 ---@param diag vim.Diagnostic
 local function urlInDiagObj(diag) return diag.user_data.lsp.codeDescription.href end
 
---------------------------------------------------------------------------------
----@alias ruleTemplate string|false|fun(diag: vim.Diagnostic): string?
+---@param diag vim.Diagnostic
+local function eslintPluginDocs(diag)
+	if not diag or not diag.code then return end
 
+	local plugin = vim.fs.dirname(tostring(diag.code))
+	local rule = vim.fs.basename(tostring(diag.code))
+
+	local pluginDocUrls = {
+		["."] = "https://eslint.org/docs/latest/rules/%s",
+		["@stencil-community"] = "https://github.com/stencil-community/stencil-eslint/blob/main/docs/%s.md",
+		["@typescript-eslint"] = "https://typescript-eslint.io/rules/%s",
+		["astro"] = "https://github.com/ota-meshi/eslint-plugin-astro/blob/main/docs/rules/%s.md",
+		["compat"] = "https://github.com/amilajack/eslint-plugin-compat/blob/main/docs/rules/%s.md",
+		["es-x"] = "https://github.com/eslint-community/eslint-plugin-es-x/blob/master/docs/rules/%s.md",
+		["eslint-plugin"] = "https://github.com/eslint-community/eslint-plugin-eslint-plugin/blob/main/docs/rules/%s.md",
+		["github"] = "https://github.com/github/eslint-plugin-github/blob/main/docs/rules/%s.md",
+		["jest"] = "https://github.com/jest-community/eslint-plugin-jest/blob/main/docs/rules/%s.md",
+		["jest-dom"] = "https://github.com/testing-library/eslint-plugin-jest-dom/blob/main/docs/rules/%s.md",
+		["jsdoc"] = "https://github.com/gajus/eslint-plugin-jsdoc/blob/main/docs/rules/%s.md",
+		["jsx-a11y"] = "https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/docs/rules/%s.md",
+		["n"] = "https://github.com/eslint-community/eslint-plugin-n/blob/master/docs/rules/%s.md",
+		["n/no-unsupported-features"] = "https://github.com/eslint-community/eslint-plugin-n/blob/master/docs/rules/no-unsupported-features/%s.md",
+		["n/prefer-global"] = "https://github.com/eslint-community/eslint-plugin-n/blob/master/docs/rules/prefer-global/%s.md",
+		["n/prefer-promises"] = "https://github.com/eslint-community/eslint-plugin-n/blob/master/docs/rules/prefer-promises/%s.md",
+		["promise"] = "https://github.com/eslint-community/eslint-plugin-promise/blob/main/docs/rules/%s.md",
+		["react"] = "https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/%s.md",
+		["react-redux"] = "https://github.com/DianaSuvorova/eslint-plugin-react-redux/blob/master/docs/rules/%s.md",
+		["security"] = "https://github.com/eslint-community/eslint-plugin-security/blob/main/docs/rules/%s.md",
+		["solid"] = "https://github.com/solidjs-community/eslint-plugin-solid/blob/main/docs/%s.md",
+		["svelte"] = "https://github.com/sveltejs/eslint-plugin-svelte/blob/main/docs/rules/%s.md",
+		["tailwindcss"] = "https://github.com/francoismassart/eslint-plugin-tailwindcss/blob/master/docs/rules/%s.md",
+		["testing_library"] = "https://github.com/testing-library/eslint-plugin-testing-library/blob/main/docs/rules/%s.md",
+		["unicorn"] = "https://github.com/sindresorhus/eslint-plugin-unicorn/blob/main/docs/rules/%s.md",
+		["vue"] = "https://github.com/vuejs/eslint-plugin-vue/blob/master/docs/rules/%s.md",
+		["wc"] = "https://github.com/43081j/eslint-plugin-wc/tree/master/docs/rules/%s.md",
+	}
+
+	for name, url in pairs(pluginDocUrls) do
+		if plugin == name then return string.format(url, rule) end
+	end
+end
+
+--------------------------------------------------------------------------------
+
+-- INFO the key must be named exactly like `diagnostic.source` (case-sensitive!)
+-- * string value: `%s` will be replaced with the rule id
+-- * function value: will be called with the diagnostic object
+-- * `false`: disable rule docs, just use the fallback
+---@alias ruleTemplate string|false|fun(diag: vim.Diagnostic): string?
 ---@type table<string, ruleTemplate>
 local M = {
 	fallback = "https://www.google.com/search?q=%s",
 
 	selene = "https://kampfkarren.github.io/selene/lints/%s.html",
 	yamllint = "https://yamllint.readthedocs.io/en/stable/rules.html#module-yamllint.rules.%s",
-	eslint = function(diag)
-		if not diag or not diag.code then return end
-
-		local plugin = vim.fs.dirname(tostring(diag.code))
-		local rule = vim.fs.basename(tostring(diag.code))
-
-		local pluginDocUrls = {
-			["."] = "https://eslint.org/docs/latest/rules/%s",
-			["@stencil-community"] = "https://github.com/stencil-community/stencil-eslint/blob/main/docs/%s.md",
-			["@typescript-eslint"] = "https://typescript-eslint.io/rules/%s",
-			["astro"] = "https://github.com/ota-meshi/eslint-plugin-astro/blob/main/docs/rules/%s.md",
-			["compat"] = "https://github.com/amilajack/eslint-plugin-compat/blob/main/docs/rules/%s.md",
-			["es-x"] = "https://github.com/eslint-community/eslint-plugin-es-x/blob/master/docs/rules/%s.md",
-			["eslint-plugin"] = "https://github.com/eslint-community/eslint-plugin-eslint-plugin/blob/main/docs/rules/%s.md",
-			["github"] = "https://github.com/github/eslint-plugin-github/blob/main/docs/rules/%s.md",
-			["jest"] = "https://github.com/jest-community/eslint-plugin-jest/blob/main/docs/rules/%s.md",
-			["jest-dom"] = "https://github.com/testing-library/eslint-plugin-jest-dom/blob/main/docs/rules/%s.md",
-			["jsdoc"] = "https://github.com/gajus/eslint-plugin-jsdoc/blob/main/docs/rules/%s.md",
-			["jsx-a11y"] = "https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/docs/rules/%s.md",
-			["n"] = "https://github.com/eslint-community/eslint-plugin-n/blob/master/docs/rules/%s.md",
-			["n/no-unsupported-features"] = "https://github.com/eslint-community/eslint-plugin-n/blob/master/docs/rules/no-unsupported-features/%s.md",
-			["n/prefer-global"] = "https://github.com/eslint-community/eslint-plugin-n/blob/master/docs/rules/prefer-global/%s.md",
-			["n/prefer-promises"] = "https://github.com/eslint-community/eslint-plugin-n/blob/master/docs/rules/prefer-promises/%s.md",
-			["promise"] = "https://github.com/eslint-community/eslint-plugin-promise/blob/main/docs/rules/%s.md",
-			["react"] = "https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/%s.md",
-			["react-redux"] = "https://github.com/DianaSuvorova/eslint-plugin-react-redux/blob/master/docs/rules/%s.md",
-			["security"] = "https://github.com/eslint-community/eslint-plugin-security/blob/main/docs/rules/%s.md",
-			["solid"] = "https://github.com/solidjs-community/eslint-plugin-solid/blob/main/docs/%s.md",
-			["svelte"] = "https://github.com/sveltejs/eslint-plugin-svelte/blob/main/docs/rules/%s.md",
-			["tailwindcss"] = "https://github.com/francoismassart/eslint-plugin-tailwindcss/blob/master/docs/rules/%s.md",
-			["testing_library"] = "https://github.com/testing-library/eslint-plugin-testing-library/blob/main/docs/rules/%s.md",
-			["unicorn"] = "https://github.com/sindresorhus/eslint-plugin-unicorn/blob/main/docs/rules/%s.md",
-			["vue"] = "https://github.com/vuejs/eslint-plugin-vue/blob/master/docs/rules/%s.md",
-			["wc"] = "https://github.com/43081j/eslint-plugin-wc/tree/master/docs/rules/%s.md",
-		}
-
-		for name, url in pairs(pluginDocUrls) do
-			if plugin == name then return string.format(url, rule) end
-		end
-	end,
+	eslint = eslintPluginDocs,
 
 	stylelint = "https://stylelint.io/user-guide/rules/%s",
 	LTeX = "https://community.languagetool.org/rule/show/%s?lang=en",
@@ -73,6 +74,7 @@ local M = {
 
 	-- urls use rule-name, not rule-id, so this is the closest we can get
 	pylint = "https://pylint.readthedocs.io/en/stable/search.html?q=%s",
+
 	Pyrefly = "https://pyrefly.org/en/docs/error-kinds/#%s",
 
 	markdownlint = function(diag)
