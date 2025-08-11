e55029cd6f145f0268b16c8eee17e0cacff3a234
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2025-08-11 14:03:26 +0200

    feat: `require("rulebook").prettifyError()` for pretty typescript errors

--- lua/rulebook/diagnostic-actions.lua
+++ lua/rulebook/diagnostic-actions.lua
@@ -1,5 +1,5 @@
 local M = {}
-local actions = {}
+local actions = {} ---@type table<string, fun(vim.Diagnostic): nil>
 local notify = require("rulebook.utils").notify
 --------------------------------------------------------------------------------
 
@@ -15,11 +15,10 @@ local function sourceUsesCodes(diag)
 	return not vim.tbl_contains(sourcesWithNoCodes, diag.source)
 end
 
----checks whether rule has id and source, as prescribed in nvim diagnostic structure
 ---@param diag vim.Diagnostic
 ---@return boolean
 ---@nodiscard
-local function validDiagObj(diag)
+local function diagHasCodeAndSource(diag)
 	if not sourceUsesCodes(diag) then return true end
 
 	local issuePlea =
@@ -57,7 +56,7 @@ function actions.lookupRule(diag)
 	local urlToOpen
 
 	if type(template) == "string" then
-		if not validDiagObj(diag) then return end
+		if not diagHasCodeAndSource(diag) then return end
 		-- `:format` can fail if there are other common `%` placeholders in the
 		-- template, thus using `:gsub`
 		urlToOpen = template:gsub("%%s", diag.code)
@@ -88,7 +87,7 @@ function actions.ignoreRule(diag)
 		notify(("No ignore comment configured for %q."):format(diag.source), "warn")
 		return
 	end
-	if not validDiagObj(diag) then return end
+	if not diagHasCodeAndSource(diag) then return end
 
 	-- parameters
 	local curLine = vim.api.nvim_get_current_line()
@@ -152,16 +151,44 @@ end
 
 ---@param diag vim.Diagnostic
 function actions.yankDiagnosticCode(diag)
-	if not validDiagObj(diag) then return end
+	if not diagHasCodeAndSource(diag) then return end
 	vim.fn.setreg("+", diag.code)
 	notify("Diagnostic code copied: \n" .. diag.code)
+end
+
+---@param diag vim.Diagnostic
+function actions.prettifyError(diag)
+	---@type Rulebook.ErrorPrettifierFunc
+	local prettifyFunc = require("rulebook.config").config.prettifyError[diag.source]
+	local errorLines = prettifyFunc(diag)
+
+	local height = math.min(#errorLines, vim.api.nvim_win_get_config(0).height - 2)
+	local title = ("  %s %s "):format(diag.source, diag.code)
+	-- using `open_floating_preview` not only saves a lot of code, but also
+	-- automatically closes potentially diagnostics floats
+	local _bufnr, winid = vim.lsp.util.open_floating_preview(errorLines, "markdown", {
+		close_events = { "CursorMoved", "CursorMovedI", "BufHidden", "LspDetach" },
+		wrap = false,
+
+		focus = true,
+		title = { { title, "ErrorMsg" } },
+		height = height,
+	})
+
+	-- FIX `title` and `height` not working
+	vim.api.nvim_win_set_config(winid, {
+		title = { { title, "ErrorMsg" } },
+		height = height,
+	})
+	-- FIX `focus` not working
+	vim.api.nvim_set_current_win(winid)
 end
 
 --------------------------------------------------------------------------------
 
 ---Selects a diagnostic in the current line. If one diagnostic, automatically
 ---selects it. If no diagnostic found, searches in the next lines.
----@param operation "lookupRule"|"ignoreRule"|"yankDiagnosticCode"
+---@param operation "lookupRule"|"ignoreRule"|"yankDiagnosticCode"|"prettifyError"
 local function selectRule(operation)
 	local config = require("rulebook.config").config
 	local lnum = vim.api.nvim_win_get_cursor(0)[1] - 1
@@ -177,31 +204,48 @@ local function selectRule(operation)
 
 		-- GUARD
 		if lnum > lastLine or lnum > startLine + config.forwSearchLines then
-			local msg = ("No diagnostics found in the next %s lines."):format(config.forwSearchLines)
+			local msg = ("No diagnostics found in the next %d lines."):format(config.forwSearchLines)
 			notify(msg, "warn")
 			return
 		end
 	end
 
-	-- remove duplicate rules
-	local uniqueRule = {}
-	for _, diag in ipairs(diagsAtLine) do
-		uniqueRule[(diag.source or "") .. (diag.code or diag.message)] = diag
+	-- remove irrelevant rules
+	-- * unconfigured rules when prettyfiying
+	-- * duplicate rules for the other operations
+	if operation == "prettifyError" then
+		diagsAtLine = vim.tbl_filter(
+			function(diag) return config.prettifyError[diag.source] ~= nil end,
+			diagsAtLine
+		)
+		if #diagsAtLine == 0 then
+			local msg = "No diagnostics found in the next "
+				.. config.forwSearchLines
+				.. " lines for which `prettifyError` is configured."
+			notify(msg, "warn")
+			return
+		end
+	else
+		local uniqueRule = {}
+		for _, diag in ipairs(diagsAtLine) do
+			uniqueRule[(diag.source or "") .. (diag.code or diag.message)] = diag
+		end
+		diagsAtLine = vim.tbl_values(uniqueRule)
 	end
-	diagsAtLine = vim.tbl_values(uniqueRule)
 
 	-- autoselect if only one diagnostic
 	if #diagsAtLine == 1 then
-		moveCursorToDiagnostic(diagsAtLine[1])
+		if operation ~= "prettifyError" then moveCursorToDiagnostic(diagsAtLine[1]) end
 		actions[operation](diagsAtLine[1])
 		return
 	end
 
 	-- select from multiple diagnostics
 	local title
-	if operation == "ignoreRule" then title = "Ignore rule:" end
-	if operation == "lookupRule" then title = "Lookup rule:" end
-	if operation == "yankDiagnosticCode" then title = "Yank diagnostic code:" end
+	if operation == "ignoreRule" then title = "Ignore rule: " end
+	if operation == "lookupRule" then title = "Lookup rule: " end
+	if operation == "yankDiagnosticCode" then title = "Yank diagnostic code: " end
+	if operation == "prettyError" then title = "Pretty error: " end
 
 	vim.ui.select(diagsAtLine, {
 		prompt = title,
@@ -230,7 +274,7 @@ local function selectRule(operation)
 		end,
 	}, function(diag)
 		if not diag then return end
-		moveCursorToDiagnostic(diag)
+		if operation ~= "prettifyError" then moveCursorToDiagnostic(diag) end
 		actions[operation](diag)
 	end)
 end
@@ -238,6 +282,7 @@ end
 function M.lookupRule() selectRule("lookupRule") end
 function M.ignoreRule() selectRule("ignoreRule") end
 function M.yankDiagnosticCode() selectRule("yankDiagnosticCode") end
+function M.prettifyError() selectRule("prettifyError") end
 
 --------------------------------------------------------------------------------
 return M
